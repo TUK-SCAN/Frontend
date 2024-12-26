08209d14d9c2e4fc11231f3bb3fa40f8083354d0
parkyugyeong <qkrb8019@gmai.com>
2024-12-26 16:54:16 +0900

    prettier plugin 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div>
-        <QueryProvider>{children}</QueryProvider>
+          <QueryProvider>{children}</QueryProvider>
         </div>
       </body>
     </html>
