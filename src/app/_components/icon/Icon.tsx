f35570b085a25bf69dbd3106f879ee22fc940e27
jinoo <jinoo0306@naver.com>
2024-12-29 17:57:26 +0900

    chore : SVG Sprite 기법으로 아이콘 정의

---
+++ src/app/_components/icon/Icon.tsx
@@ -0,0 +1,22 @@
+interface IconProps {
+  id: string
+  width?: number
+  height?: number
+  className?: string
+  color?: string
+}
+
+const Icon = ({ id, width, height, className }: IconProps) => {
+  return (
+    <svg
+      {...(width ? { width: `${width}px` } : {})}
+      {...(height ? { height: `${height}px` } : {})}
+      className={`fill-current ${className || ''}`}
+      aria-hidden="true"
+    >
+      <use xlinkHref={`#${id}`} />
+    </svg>
+  )
+}
+
+export default Icon
