b6cc8906fb22aa157199491f6cda3b5ef6fc05eb
parkyugyeong <qkrb8019@gmai.com>
2025-01-01 09:36:53 +0900

    배너 퍼블리싱 완료

---
+++ src/app/_components/Banner1.tsx
@@ -0,0 +1,24 @@
+import Icon from './icon/Icon'
+
+const Banner1 = () => {
+  return (
+    <div className="flex h-[200px] w-[1423px] items-center justify-center rounded-lg bg-blue-primary text-white">
+      <div className="flex items-center gap-2 space-x-6">
+        <div>
+          <Icon id="logo" width={150} height={66} />
+        </div>
+        <div className="text-left">
+          <p className="font-pretendard text-[28px] font-semibold leading-[1.5]">
+            TOOK을 통해 <br /> 고품질 스캔 파일을 이용해보세요!
+          </p>
+          <p className="mt-2 font-pretendard text-[16px] font-normal leading-[1.5]">
+            툭은 안전하고 편리한 스캔서비스를 제공합니다. <br />
+            지금 스캔 신청하기를 통해 빠르게 견적을 확인해보세요.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner1
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Banner from './_components/Banner'
+import Banner1 from './_components/Banner1'
 import BigCheckIcon from './_components/BigCheckIcon'
 import SmallCheckIcon from './_components/SmallCheckIcon'
+import Banner2 from './_components/Banner2'
+import Banner3 from './_components/Banner3'
 
 const App = () => {
   return (
     <div>
-      <Banner></Banner>
+      <Banner1></Banner1>
+      <Banner2></Banner2>
+      <Banner3></Banner3>
       <SmallCheckIcon></SmallCheckIcon>
       <BigCheckIcon></BigCheckIcon>
       hi
