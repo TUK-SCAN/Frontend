d201646aa6a4bfbafc6fa36c28a7d0440ecec5ec
jinoo <jinoo0306@naver.com>
2025-01-01 22:27:39 +0900

    merge : design/#19 <- develop rebase

---
+++ src/app/_components/ui/InputField.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import Icon from '@/app/_components/icon/Icon'
+
+interface InputFieldProps {
+  type: 'simple' | 'number' | 'suffix' | 'search' | 'password'
+  value?: string
+  inputRef?: React.RefObject<HTMLInputElement>
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder?: string
+  helperText?: string
+  disabled?: boolean
+  isError?: boolean
+  isSuccess?: boolean
+}
+
+const InputField = ({
+  type,
+  value,
+  inputRef,
+  onChange,
+  placeholder,
+  helperText,
+  disabled,
+  isError,
+  isSuccess,
+}: InputFieldProps) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const [isPasswordVisible, setIsPasswordVisible] = useState(
+    type !== 'password'
+  )
+
+  return (
+    <div className="flex w-96 flex-col gap-2">
+      <div
+        className={clsx(
+          'flex h-[3.125rem] flex-row items-center gap-1 rounded-lg bg-blue-secondary px-[1.12rem] py-3',
+          disabled && 'cursor-not-allowed bg-black-200 text-black-800',
+          isError && !disabled && 'border-status-error rounded-lg border',
+          isSuccess && !disabled && 'border-status-success border'
+        )}
+      >
+        {type === 'search' && (
+          <Icon
+            id="search"
+            width={16}
+            height={16}
+            className={clsx(
+              disabled && 'text-black-800',
+              isFocused ? 'text-blue-primary' : 'text-black-600'
+            )}
+          />
+        )}
+
+        <input
+          ref={inputRef}
+          value={value}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={onChange}
+          disabled={disabled}
+          type={isPasswordVisible ? 'text' : 'password'}
+          placeholder={placeholder}
+          className="w-full bg-transparent text-black-800 outline-none"
+        />
+        {type === 'suffix' && <p className="text-black-400">@gmail.com</p>}
+        {type === 'password' && (
+          <button onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+            <Icon
+              id={isPasswordVisible ? 'openEyes' : 'closeEyes'}
+              width={24}
+              height={24}
+              className="text-black-800"
+            />
+          </button>
+        )}
+      </div>
+
+      {helperText && (
+        <span
+          className={clsx(
+            'text-[0.75rem]',
+            isError && 'text-status-error',
+            isSuccess && 'text-status-success'
+          )}
+        >
+          {helperText}
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default InputField
