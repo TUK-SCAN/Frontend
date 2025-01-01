77833742e8778222944c95a88882fee852d279ca
parkyugyeong <qkrb8019@gmai.com>
2025-01-01 09:36:53 +0900

    배너 퍼블리싱 완료

---
+++ src/app/_components/Banner2.tsx
@@ -0,0 +1,24 @@
+import Icon from './icon/Icon'
+
+const Banner2 = () => {
+  return (
+    <div className="flex h-[156px] w-[1423px] items-center justify-start rounded-lg bg-blue-primary p-3 text-white">
+      <div className="flex items-center gap-1.5 space-x-6">
+        <div>
+          <Icon id="logo" width={80} height={36} />
+        </div>
+        <div className="text-left">
+          <p className="font-pretendard text-[20px] font-normal leading-[1.5]">
+            TOOK을 통해 <br /> 고품질 스캔 파일을 이용해보세요!
+          </p>
+          <p className="mt-2 font-pretendard text-[12px] font-normal leading-[1.5]">
+            툭은 안전하고 편리한 스캔서비스를 제공합니다. <br />
+            지금 스캔 신청하기를 통해 빠르게 견적을 확인해보세요.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner2
---
+++ src/app/_components/Banner3.tsx
@@ -0,0 +1,28 @@
+import Icon from './icon/Icon'
+
+const Banner3 = () => {
+  return (
+    <div className="flex h-[200px] w-[1423px] items-center justify-center rounded-lg bg-blue-primary p-8 text-white">
+      {/* Wrapper for Icon and Text */}
+      <div className="flex h-full w-[1135px] items-center justify-between px-6">
+        {/* Logo Section */}
+
+        {/* Text Section */}
+        <div className="flex flex-col justify-center text-left">
+          <p className="font-pretendard text-[32px] font-bold leading-[1.5]">
+            신청하기
+          </p>
+          <p className="mt-2 font-pretendard text-[12px] leading-[1.5]">
+            툭은 안전하고 편리한 스캔서비스를 제공합니다. <br />
+            지금 스캔 신청하기를 통해 빠르게 견적을 확인해보세요.
+          </p>
+        </div>
+        <div className="flex items-center">
+          <Icon id="logo" width={150} height={66} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner3
