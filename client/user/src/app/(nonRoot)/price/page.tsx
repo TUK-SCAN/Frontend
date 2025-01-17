43a545c25929ce5ac1a607e5705914ee5dbb244a
parkyugyeong <qkrb8019@gmai.com>
2025-01-17 23:09:54 +0900

    new file:   client/user/src/app/(nonRoot)/privacy/Agreement3/page.tsx

--- client/user/src/app/(nonRoot)/price/page.tsx
+++ client/user/src/app/(nonRoot)/price/page.tsx
@@ -1,5 +1,5 @@
-import Banner from "@tookscan/components/ui/Banner/Banner";
-import Icon from "@tookscan/components/ui/Icon/Icon";
+import Banner from "@tookscan/components/ui/Banner/Banner"
+import Icon from "@tookscan/components/ui/Icon/Icon"
 
 const Price = () => {
   return (
