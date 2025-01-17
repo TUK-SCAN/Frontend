ac33258e02f3c93c3a7b67f89d0cd71d3c69bb1d
parkyugyeong <qkrb8019@gmai.com>
2025-01-17 23:09:54 +0900

    new file:   client/user/src/app/(nonRoot)/privacy/Agreement3/page.tsx

--- client/user/src/app/(nonRoot)/price/page.tsx
+++ client/user/src/app/(nonRoot)/price/page.tsx
@@ -1,7 +1,5 @@
-
-import Banner from "@tookscan/components/ui/Banner/Banner"
-import Icon from "@tookscan/components/ui/Icon/Icon"
-
+import Banner from '@tookscan/components/ui/Banner/Banner'
+import Icon from '@tookscan/components/ui/Icon/Icon'
 
 const Price = () => {
   return (
@@ -103,75 +101,73 @@ const Price = () => {
             </div>
           </div>
         </div>
-       
-        <div className=" w-[600px] overflow-hidden rounded-lg ">
-  <table className="w-full border-collapse !border !border-gray-600 text-[18px]">
-    <thead>
-      <tr>
-        <th className="bg-blue-secondary borderpx-4 py-2 text-center align-middle text-blue-primary w-[150px] h-[56px]">
-          항목
-        </th>
-        <th className="!border !border-gray-500 text-gray-500  px-4 py-2 text-center align-middle w-[250px] h-[56px]">
-          상세 내용
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td className="bg-blue-secondary border px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          품질
-        </td>
-        <td className="border  text-gray-500 px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          고해상도 컬러 스캔
-        </td>
-      </tr>
-      <tr>
-        <td className="bg-blue-secondary border px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          DPI
-        </td>
-        <td className="border text-gray-500 px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          최대 600 DPI
-        </td>
-      </tr>
-      <tr>
-        <td className="bg-blue-secondary border  px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          색감
-        </td>
-        <td className="border text-gray-500  px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          24비트 컬러
-        </td>
-      </tr>
-      <tr>
-        <td className="bg-blue-secondary border px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          선명도
-        </td>
-        <td className="border text-gray-500 px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          자동 이미지 보정 및 선명한 텍스트 출력 지원
-        </td>
-      </tr>
-      <tr>
-        <td className="bg-blue-secondary border  px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          검수작업
-        </td>
-        <td className="border text-gray-500 px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          빈 페이지 제거 및 배경틈 보정 기능
-        </td>
-      </tr>
-      <tr>
-        <td className="bg-blue-secondary border px-4 py-2 text-center align-middle text-blue-primary w-[204px] h-[56px]">
-          기울기보정
-        </td>
-        <td className="border text-gray-500 px-4 py-2 text-center align-middle w-[346px] h-[56px]">
-          자동 기울기 보정 및 잘못된 정렬 수정
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
 
-</div>
-</div>
-  
+        <div className="w-[600px] overflow-hidden rounded-lg">
+          <table className="w-full border-collapse !border !border-gray-600 text-[18px]">
+            <thead>
+              <tr>
+                <th className="borderpx-4 h-[56px] w-[150px] bg-blue-secondary py-2 text-center align-middle text-blue-primary">
+                  항목
+                </th>
+                <th className="h-[56px] w-[250px] !border !border-gray-500 px-4 py-2 text-center align-middle text-gray-500">
+                  상세 내용
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  품질
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  고해상도 컬러 스캔
+                </td>
+              </tr>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  DPI
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  최대 600 DPI
+                </td>
+              </tr>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  색감
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  24비트 컬러
+                </td>
+              </tr>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  선명도
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  자동 이미지 보정 및 선명한 텍스트 출력 지원
+                </td>
+              </tr>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  검수작업
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  빈 페이지 제거 및 배경틈 보정 기능
+                </td>
+              </tr>
+              <tr>
+                <td className="h-[56px] w-[204px] border bg-blue-secondary px-4 py-2 text-center align-middle text-blue-primary">
+                  기울기보정
+                </td>
+                <td className="h-[56px] w-[346px] border px-4 py-2 text-center align-middle text-gray-500">
+                  자동 기울기 보정 및 잘못된 정렬 수정
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
-export default Price;
+export default Price
