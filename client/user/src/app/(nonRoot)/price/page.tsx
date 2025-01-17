17710e0225af5be91bde4e3ce8fe4404b762d987
parkyugyeong <qkrb8019@gmai.com>
2025-01-18 01:24:22 +0900

    design: wip

--- client/user/src/app/(nonRoot)/price/page.tsx
+++ client/user/src/app/(nonRoot)/price/page.tsx
@@ -101,6 +101,70 @@ const Price = () => {
             </div>
           </div>
         </div>
+       
+        <table className="text-[18px] w-full border-collapse border border-gray-300 text-gray-700 rounded-lg">
+  <thead>
+    <tr>
+      <th className=" bg-blue-secondary border border-gray-300 px-4 py-2 text-blue-primary w-[204px] h-[56px] ">
+        항목
+      </th>
+      <th className="border border-gray-300 px-4 py-2 w-[346px] h-[56px] ">
+        상세 내용
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px]">
+        품질
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px]">
+        고해상도 컬러 스캔
+      </td>
+    </tr>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px]">
+        DPI
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px]">
+        최대 600 DPI
+      </td>
+    </tr>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px]">
+        색감
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px]">
+        24비트 컬러
+      </td>
+    </tr>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px]">
+        선명도
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px]">
+        자동 이미지 보정 및 선명한 텍스트 출력 지원
+      </td>
+    </tr>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px]">
+        검수작업
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px]">
+        빈 페이지 제거 및 배경틈 보정 기능
+      </td>
+    </tr>
+    <tr>
+      <td className="bg-blue-secondary border border-gray-300 px-4 py-2 text-center text-blue-primary w-[204px] h-[56px] ">
+        기울기보정
+      </td>
+      <td className="border border-gray-300 px-4 py-2 text-center w-[346px] h-[56px] ">
+        자동 기울기 보정 및 잘못된 정렬 수정
+      </td>
+    </tr>
+  </tbody>
+</table>
+
       </div>
     </div>
   )
