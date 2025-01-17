eb40cd013e224dc873a0a2f2cc8fd9fc073988bf
jinoo <jinoo0306@naver.com>
2025-01-18 06:49:11 +0900

    file : turbo 프로젝트 삭제

--- packages/components/eslint.config.mjs
+++ packages/components/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@shared/eslint/eslint.config.mjs";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
