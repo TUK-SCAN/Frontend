40360dc7aea5b1e6b3c2b0f8c248deaa5dc3a8e4
parkyugyeong <qkrb8019@gmai.com>
2025-01-21 07:56:53 +0900

    design: 개인정보 동의페이지 퍼블리싱

---
+++ client/user/src/app/(nonRoot)/privacy/Agreement1/page.tsx
@@ -0,0 +1,62 @@
+const Agreement1 = () => {
+  return (
+    <div className="p-[1.5rem]">
+      {/* 첫 번째 글 상자 */}
+      <div className="mb-[1.5rem] flex w-[22rem] items-center justify-start">
+        <div>
+          <h2 className="mb-4 text-[1.25rem] font-bold text-black">
+            개인정보 수집목적 및 이용목적
+          </h2>
+          <p className="text-[1rem] text-gray-700">
+            회사는 다음의 목적을 위하여 개인정보를 처리합니다. 처리하고 있는
+            개인정보는 다음의 목적 이외의 용도로는 이용되지 않으며, 이용 목적이
+            변경되는 경우에는 개인정보보호법 제18조에 따라 별도의 동의를 받는 등
+            필요한 조치를 이행할 예정입니다.
+          </p>
+        </div>
+      </div>
+
+      {/* 두 번째 글 상자 */}
+      <div className="flex items-center justify-start">
+        <div>
+          <ul className="list-decimal pl-6 text-[1rem] text-gray-700">
+            <li className="mb-2">
+              홈페이지 회원 가입 및 관리
+              <p className="text-gray-600">
+                회원 가입 의사 확인, 회원제 서비스 제공에 따른 본인 식별·인증,
+                회원자격 유지·관리, 제한적 본인확인제 시행에 따른 본인확인, 가입
+                및 가입횟수 제한, 서비스 부정 이용 방지, 비회원 주문 및
+                식별·인증, 14세 이상 연령 확인, 각종 고지·통지, 고충 처리를
+                목적으로 개인정보를 처리합니다.
+              </p>
+            </li>
+            <li className="mb-2">
+              서비스 제공
+              <p className="text-gray-600">
+                물품 배송, 서비스 제공, 계약서 및 청구서 발송, 콘텐츠 제공,
+                맞춤서비스 제공, 본인인증, 연령인증, 요금 결제 및 정산, 비회원
+                주문조회를 목적으로 개인정보를 처리합니다.
+              </p>
+            </li>
+            <li className="mb-2">
+              고충 처리
+              <p className="text-gray-600">
+                민원인의 신원 확인, 민원사항 확인, 사실조사를 위한 연락·통지,
+                처리 결과 통보의 목적으로 개인정보를 처리합니다.
+              </p>
+            </li>
+            <li>
+              마케팅 (선택사항)
+              <p className="text-gray-600">
+                이벤트 정보제공, 광고 정보제공, 마케팅 활용 목적으로 개인정보를
+                처리합니다.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Agreement1
--- client/user/src/app/(nonRoot)/privacy/layout.tsx
+++ client/user/src/app/(nonRoot)/privacy/layout.tsx
@@ -1,35 +1,62 @@
-// components/PrivacyLayout.tsx
+'use client'
+
 import Banner from '@tookscan/components/ui/Banner/Banner'
+import { useState } from 'react'
 import { LayoutProps } from '../../../types/common'
 
 const PrivacyLayout = ({ children }: LayoutProps) => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(0)
+
+  const menuItems = [
+    { label: '개인정보 수집목적 및 이용목적', link: './privacy/Agreement1' },
+    { label: '수집하는 개인정보 항목', link: './Agreement2' },
+    { label: '서비스 부정이용자 처리방안', link: './Agreement3' },
+    {
+      label: '서비스 이용과정에서 수집되는 정보',
+      link: './Agreement4',
+    },
+    { label: '개인정보의 수집 방법', link: './Agreement5' },
+  ]
+
   return (
-    <div>
+    <div className="min-h-screen bg-blue-secondary px-8 py-12">
       <Banner type={3}></Banner>
-      <div className="bg-blue-secondary"></div>
-      <div className="mb-4 text-lg font-semibold">약관을 확인해주세요</div>
-      <div className="mb-6 flex gap-4">
-        {[
-          { label: '개인정보 수집목적 및 이용목적', link: './privacy1/page' },
-          { label: '수집하는 개인정보 항목', link: './privacy2/page' },
-          { label: '서비스 부정이용자 처리방안', link: './privacy3/page' },
-          {
-            label: '서비스 이용과정에서 수집되는 정보',
-            link: './privacy4/page',
-          },
-          { label: '개인정보의 수집 방법', link: './privacy5/page' },
-        ].map((item, index) => (
-          <button
-            key={index}
-            className="max-w-[150px] truncate rounded-md border border-gray-300 bg-white px-6 py-2 text-sm font-medium"
-            onClick={() => (window.location.href = item.link)}
-            title={item.label} // 툴팁 제공
-          >
-            {item.label}
-          </button>
-        ))}
+      <div className="flex flex-col items-center justify-center">
+        <div className="py-[6.25rem]">
+          <div className="my-[3.5rem]">
+            <div className="">
+              <h1 className="text-blue-primary">이용약관 | 개인정보처리방침</h1>
+              <h1 className="mt-[0.8rem] text-[2rem] font-semibold text-black">
+                약관을 확인해주세요
+              </h1>
+            </div>
+            <div className="flex flex-col items-start justify-center">
+              <div className="my-[1.5rem] flex justify-center gap-[0.5rem]">
+                {menuItems.map((item, index) => (
+                  <button
+                    key={index}
+                    className={`h-[2.2rem] w-[10rem] items-center justify-center truncate rounded-full border px-[1.25rem] py-[0.3rem] text-sm font-medium transition-all duration-300 ${
+                      activeIndex === index
+                        ? 'bg-blue-primary text-white'
+                        : 'border-blue-primary text-blue-primary'
+                    }`}
+                    onClick={() => {
+                      setActiveIndex(index)
+                      window.location.href = item.link
+                    }}
+                    title={item.label} // 말줄임표를 볼 수 있는 툴팁
+                  >
+                    {item.label}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="w-[72rem] rounded-md bg-white p-6 pt-[3.2rem] shadow-md">
+            {children}
+          </div>
+        </div>
       </div>
-      <div className="rounded-md bg-white p-6 shadow-md">{children}</div>
     </div>
   )
 }
