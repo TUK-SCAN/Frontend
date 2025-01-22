5d9f7544e9269c58d60a778be4686db250910366
parkyugyeong <qkrb8019@gmai.com>
2025-01-23 05:26:41 +0900

    feat: 버튼효과 수정

--- client/user/src/app/(nonRoot)/privacy/Agreement4/page.tsx
+++ client/user/src/app/(nonRoot)/privacy/Agreement4/page.tsx
@@ -6,11 +6,14 @@ const Agreement4 = () => {
           <h2 className="mb-4 text-[1.25rem] font-bold text-black">
             서비스 이용과정에서 수집될 수 있는 정보
           </h2>
-          <p className="text-[1rem] text-gray-700">
-            - 이용자의 브라우저 종류 및 OS, 접속 IP 정보, 접속로그, 쿠키 <br />-
-            (모바일기기 이용 시) 단말기 정보, 단말 OS 정보 <br />-
-            구매/결제/취소/환불/상담 내역
-          </p>
+          <ul className="list-disc space-y-2 pl-4 text-[1rem] text-gray-700">
+            {' '}
+            <li>
+              이용자의 브라우저 종류 및 OS, 접속 IP 정보, 접속로그, 쿠키
+            </li>{' '}
+            <li>(모바일기기 이용 시) 단말기 정보, 단말 OS 정보</li>{' '}
+            <li>구매/결제/취소/환불/상담 내역</li>
+          </ul>
         </div>
       </div>
     </div>
