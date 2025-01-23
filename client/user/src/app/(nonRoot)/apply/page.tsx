4a04e503bda7d6252bf48d52e3870b84cb5cbb55
jinoo <jinoo0306@naver.com>
2025-01-24 00:58:28 +0900

    design : 신청 페이지 레이아웃과 기본 페이지 구성

--- client/user/src/app/(nonRoot)/apply/page.tsx
+++ client/user/src/app/(nonRoot)/apply/page.tsx
@@ -1,17 +1,62 @@
 'use client'
-import { useState } from 'react'
-import { Books } from '@/types/common'
-import Purchase from '@/app/(nonRoot)/apply/_components/Purchase'
-const Apply = () => {
-  const [books, setBooks] = useState<Books[]>([])
-  const handleRemoveBook = (id: number) => {
-    setBooks((prev) => prev.filter((book) => book.id !== id))
-  }
+
+import { useEffect } from 'react'
+import clsx from 'clsx'
+import Banner from '@tookscan/components/ui/Banner/Banner'
+import { StepSheet, PageInfo, Purchase } from './_components/index'
+import {
+  ApplyProvider,
+  useApplyContext,
+} from '@/app/(nonRoot)/apply/_contexts/ApplyContext'
+import Icon from '@tookscan/components/ui/Icon/Icon'
+
+const ApplyContent = () => {
+  const { books } = useApplyContext()
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (books.length > 0) {
+        event.preventDefault()
+      }
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload)
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [books])
+
   return (
-    <div>
-      <Purchase books={books} onDelete={handleRemoveBook} />
-    </div>
+    <>
+      <Banner type={3} />
+      <div className={clsx('flex w-full flex-col gap-6 px-36 py-20')}>
+        <PageInfo />
+        <div className={clsx('flex w-full flex-row items-start gap-12')}>
+          <StepSheet />
+
+          {books.length > 0 ? (
+            <Purchase />
+          ) : (
+            <div className="flex h-full w-full flex-col items-center justify-center">
+              <div className="flex h-full flex-col items-center justify-center">
+                <Icon id="logo" className="text-blue-primary" />
+                <p className="mt-8 text-2xl font-bold text-blue-primary">
+                  Took Scan을 통해 고품질 스캔 파일을 이용해 보세요!
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   )
 }
+
+const Apply = () => (
+  <ApplyProvider>
+    <ApplyContent />
+  </ApplyProvider>
+)
 
 export default Apply
