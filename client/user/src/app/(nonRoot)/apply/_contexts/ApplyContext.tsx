af8a097c7644917bf694a5faf98506ac264b6e45
jinoo <jinoo0306@naver.com>
2025-01-24 01:01:48 +0900

    feat : 전역 상태관리를 위해 ContextAPI 구성

---
+++ client/user/src/app/(nonRoot)/apply/_contexts/ApplyContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useRef, ReactNode, useState } from 'react'
+import {
+  Books,
+  ShippingInfo,
+  initialShippingInfo,
+  Terms,
+  initialTerms,
+} from '@/types/book'
+
+interface ApplyContextType {
+  pageIndex: number
+  setPageIndex: React.Dispatch<React.SetStateAction<number>>
+  books: Books[]
+  setBooks: React.Dispatch<React.SetStateAction<Books[]>>
+  shippingInfoRef: React.MutableRefObject<ShippingInfo>
+  updateShippingInfo: (key: keyof ShippingInfo, value: string) => void
+  terms: Terms
+  setTerms: (terms: Terms) => void
+}
+
+const ApplyContext = createContext<ApplyContextType | undefined>(undefined)
+
+export const ApplyProvider = ({ children }: { children: ReactNode }) => {
+  const [pageIndex, setPageIndex] = useState<number>(0)
+  const [books, setBooks] = useState<Books[]>([])
+  const shippingInfoRef = useRef<ShippingInfo>(initialShippingInfo)
+  const [terms, setTerms] = useState<Terms>(initialTerms)
+
+  const updateShippingInfo = (key: keyof ShippingInfo, value: string) => {
+    shippingInfoRef.current[key] = value
+  }
+
+  return (
+    <ApplyContext.Provider
+      value={{
+        pageIndex,
+        setPageIndex,
+        books,
+        setBooks,
+        shippingInfoRef,
+        updateShippingInfo,
+        terms,
+        setTerms,
+      }}
+    >
+      {children}
+    </ApplyContext.Provider>
+  )
+}
+
+export const useApplyContext = () => {
+  const context = useContext(ApplyContext)
+  if (!context) {
+    throw new Error('useApplyContext must be used within an ApplyProvider')
+  }
+  return context
+}
