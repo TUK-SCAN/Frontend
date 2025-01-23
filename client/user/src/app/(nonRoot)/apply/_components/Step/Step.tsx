7a6433827f297365e61577b11204a10a77eef278
jinoo <jinoo0306@naver.com>
2025-01-24 00:59:54 +0900

    design : purchase 컴포넌트 내부 반복되는 책 정보 컴포넌트 퍼블리싱

---
+++ client/user/src/app/(nonRoot)/apply/_components/Step/Step.tsx
@@ -0,0 +1,23 @@
+import { useApplyContext } from '@/app/(nonRoot)/apply/_contexts/ApplyContext'
+import { ScanInfo, ShippingInfo, TermsAgreement } from '../index'
+import clsx from 'clsx'
+
+const Step = {
+  0: () => <ScanInfo />,
+  1: () => <ShippingInfo />,
+  2: () => <TermsAgreement />,
+}
+
+const StepSheet = () => {
+  const { pageIndex } = useApplyContext()
+  const StepComponent = Step[pageIndex as keyof typeof Step] // 컴포넌트 함수 가져오기
+  return StepComponent ? (
+    <div className={clsx('flex flex-col gap-12 rounded-3xl bg-white p-12')}>
+      <StepComponent />
+    </div>
+  ) : (
+    <div>잘못된 페이지입니다.</div>
+  )
+}
+
+export default StepSheet
