51ac1d43ed822ca5e1b3bb462d9fead7e45e85d1
jinoo <jinoo0306@naver.com>
2025-01-25 10:38:35 +0900

    design : 아코디언 컴포넌트 디자인 수정

--- packages/components/ui/Accordion.tsx
+++ packages/components/ui/Accordion.tsx
@@ -1,49 +1,51 @@
-"use client";
-import React, { useState } from "react";
-import Icon from "./Icon/Icon";
-import CheckButton from "./CheckButton";
+'use client'
+import React, { useState } from 'react'
+import Icon from './Icon/Icon'
+import CheckButton from './CheckButton'
+import clsx from 'clsx'
 
 interface AccordionProps {
-  title: string;
-  contents: string;
+  title: string
+  contents?: string
+  isCheck: boolean
+  onClick: () => void
 }
-const Accordion = ({ title, contents }: AccordionProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isCheck, setIsCheck] = useState(false);
+const Accordion = ({ title, contents, isCheck, onClick }: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="w-[847px] rounded-lg border border-gray-300 shadow-sm">
-      <div className="flex h-[56px] items-center justify-between px-4 py-2">
-        <CheckButton
-          size="lg"
-          isChecked={isCheck}
-          onClick={() => {
-            setIsCheck(!isCheck);
-          }}
-        />
-        <span className="flex-1 text-start text-lg text-gray-800">{title}</span>
-        <button
-          className="flex h-6 w-6 items-center justify-center text-gray-500"
-          onClick={() => setIsOpen(!isOpen)}
+    <div className="bg-blue-secondary w-full rounded-lg">
+      <div className="flex items-center justify-between py-5 pl-2 pr-6 font-semibold">
+        <CheckButton size="lg" isChecked={isCheck} onClick={onClick} />
+        <h2 className="flex-1 text-start text-lg text-gray-800">{title}</h2>
+        {contents && (
+          <button
+            className="flex h-6 w-6 items-center justify-center text-gray-500"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <Icon
+              id="arrow"
+              className={clsx(
+                'transition-transform',
+                isOpen ? 'rotate-180' : 'rotate-0'
+              )}
+            />
+          </button>
+        )}
+      </div>
+      {contents && (
+        <div
+          className={`overflow-hidden transition-[max-height] duration-300 ${
+            isOpen ? 'max-h-40' : 'max-h-0'
+          }`}
         >
-          <Icon
-            id="arrow"
-            className={`transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`}
-          />
-        </button>
-      </div>
-
-      <div
-        className={`overflow-hidden transition-[max-height] duration-300 ${
-          isOpen ? "max-h-40" : "max-h-0"
-        }`}
-      >
-        <div className="border-t border-gray-300 p-4">
-          <p className="text-{14px} text-gray-600">{contents}</p>
+          <div className="px-16 pb-4">
+            <p className="text-{14px} text-gray-600">{contents}</p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default Accordion;
+export default Accordion
